970257d22a74cf8cd9932a31596dbe11cad7c34c
Morayo19 <morayosufian@gmail.com>
2023-06-22 18:15:35 -0700

    task3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -9,11 +9,10 @@
 
 void print_numbers(void)
 {
-	int num = 0;
-
-	do {
-		_putchar(num + 48);
-		num++;
-	} while (num >= 0 && num <= 9);
-	_putchar('\n');
+	int i;
+	for (i = 0; i<=9;i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
 }
