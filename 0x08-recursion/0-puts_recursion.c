157ff104a90abf8784ebfd02537ab90974064df8
Morayo19 <morayosufian@gmail.com>
2023-07-05 11:54:24 -0700

    Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion - print  string
+ * @s: the character
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	_putchar('\n');
+}i
