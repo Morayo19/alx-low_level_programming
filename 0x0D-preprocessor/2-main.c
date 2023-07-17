36d949303ac2d20df613972f3c5fb39e2e1f8d49
Morayo19 <morayosufian@gmail.com>
2023-07-17 13:31:28 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,11 +2,11 @@
 /**
  * main- a program that retuens the name of the file it was compiled from
  *
- * retun (0)
+ * retun: 0 (success)
  */
 int main(void)
 {
 
-printf("%s\n",__FILE__);
-	return 0;
+	printf("%s\n",__FILE__);
+	return (0);
 }
